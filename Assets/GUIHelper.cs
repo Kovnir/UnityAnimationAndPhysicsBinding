83592fe2a8b8846bb6153405131460440f3c916a
Kovnir Home <kovnir.alik@gmail.com>
2020-07-25 19:28:34 +0300

    Clean up gui helper

--- Assets/GUIHelper.cs
+++ Assets/GUIHelper.cs
@@ -6,19 +6,9 @@ using UnityEngine;
 public class GUIHelper : MonoBehaviour
 {
     [SerializeField]
-    private Animator animator;
-    [SerializeField]
     private AnimationBlender animationBlender;
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-//            animationBlender.ResetAngles();
-        }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            animator.SetTrigger("Next");
-        }
         if (Input.GetKeyDown(KeyCode.R))
         {
             animationBlender.ResetAngles();
